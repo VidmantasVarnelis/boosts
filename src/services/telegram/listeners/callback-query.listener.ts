VidmantasVarnelis/trezor

because I’m Batman.

src/services/telegram/listeners/callback-query.listener.ts
@@ -1,0 +1,127 @@
+import { PrismaSubscriptionRepository, PrismaUserRepository } from '@repo/database/repositories';
+import { CallbackQuery } from 'telegraf/types';
+import { ApplicationPlatform } from '@repo/database';
+import { DonateCommand, SubscriptionCommand, UserWalletCommand } from '../commands';
+import { GeneralMessages } from '../messages';
+import { START_MENU } from '../lib/menus';
+import {
+  userExpectingDonation,
+  userExpectingAddWalletAddress,
+  userExpectingDeleteWalletAddress,
+} from '../lib/waiting-stage';
+import { TelegramService } from '../telegram.service';
+import { Config, DonationEnum } from '../../../constants';
+import { CallbackQueryDataEnum } from '../constants';
+
+export class CallbackQueryListener {
+  private readonly userWalletCommand: UserWalletCommand;
+  private readonly subscriptionCommand: SubscriptionCommand;
+  private readonly donateCommand: DonateCommand;
+
+  constructor(
+    private readonly config: Config,
+    private readonly telegramService: TelegramService,
+    private readonly prismaUserRepository: PrismaUserRepository,
+    private readonly prismaSubscriptionRepository: PrismaSubscriptionRepository,
+  ) {
+    this.donateCommand = new DonateCommand(
+      this.config,
+      this.telegramService,
+      this.prismaUserRepository,
+      this.prismaSubscriptionRepository,
+    );
+    this.subscriptionCommand = new SubscriptionCommand(
+      this.config,
+      this.telegramService,
+      this.prismaUserRepository,
+      this.prismaSubscriptionRepository,
+    );
+    this.userWalletCommand = new UserWalletCommand(
+      this.config,
+      this.telegramService,
+      this.prismaUserRepository,
+    );
+  }
+
+  public start() {
+    this.telegramService.onCallbackQuery(async (query) => {
+      const chatId = query.chat?.id;
+      const message = query.callbackQuery.message;
+
+      if (!chatId || !message) {
+        return;
+      }
+
+      const data = (query.callbackQuery as CallbackQuery & { data: string | undefined }).data;
+
+      try {
+        switch (data) {
+          case CallbackQueryDataEnum.MY_WALLET:
+            await this.userWalletCommand.addButtonHandler(message.chat.id, message.message_id);
+            break;
+          case CallbackQueryDataEnum.SHOW_PRIVATE_KEY:
+            await this.userWalletCommand.showPrivateKey(message.chat.id, message.message_id);
+            break;
+          case CallbackQueryDataEnum.UPGRADE:
+            await this.subscriptionCommand.addButtonHandler(message.chat.id, message.message_id);
+            break;
+          case CallbackQueryDataEnum.UPGRADE_HOBBY:
+            await this.subscriptionCommand.upgradePlan(
+              message.chat.id,
+              message.message_id,
+              'HOBBY',
+            );
+            break;
+          case CallbackQueryDataEnum.UPGRADE_PRO:
+            await this.subscriptionCommand.upgradePlan(message.chat.id, message.message_id, 'PRO');
+            break;
+          case CallbackQueryDataEnum.DONATE:
+            await this.donateCommand.addButtonHandler(message.chat.id, message.message_id);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.MINIMUM}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.MINIMUM);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.LOW}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.LOW);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.MEDIUM}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.MEDIUM);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.HIGH}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.HIGH);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.PREMIUM}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.PREMIUM);
+            break;
+          case CallbackQueryDataEnum.BACK_TO_MAIN_MENU: {
+            const user = await this.prismaUserRepository.getById(chatId.toString());
+
+            userExpectingAddWalletAddress.delete(chatId);
+            userExpectingDonation.delete(chatId);
+            userExpectingDeleteWalletAddress.delete(chatId);
+
+            await this.telegramService.editMessageText(
+              chatId,
+              message.message_id,
+              GeneralMessages.startMessage(
+                user,
+                ApplicationPlatform.BOOSTS,
+                this.config.freeDailyMessages.get(chatId.toString())?.count ?? 0,
+              ),
+              {
+                replyMarkup: START_MENU,
+                mode: 'HTML',
+              },
+            );
+
+            break;
+          }
+          default:
+            await this.telegramService.sendMessage(chatId, 'Unknown command.');
+        }
+      } catch {
+        await this.telegramService.sendMessage(chatId, GeneralMessages.botInternalServerError());
+      }
+    });
+  }
+}

src/services/telegram/messages/general.messages.ts
@@ -1,0 +1,108 @@
+import dedent from 'dedent';
+import { MAX_FREE_DAILY_MESSAGES } from '../../../constants/pricing';
+import { UserPrisma } from '../../../typings';
+import { ApplicationPlatform } from '@repo/database';
+
+export class GeneralMessages {
+  public static startMessage(
+    user: UserPrisma,
+    platform: ApplicationPlatform,
+    dailyMessages: number,
+  ): string {
+    const subscription = user?.userSubscriptions?.find((sub) => sub.platform === platform);
+    const plan = subscription?.plan || 'FREE';
+
+    const messageText = dedent(`
+      Better Call SOL | BOOSTS!
+      You're currently on <b>${plan}</b> plan${plan === 'FREE' ? `\nFor Free plan you have every 6 hours ${dailyMessages}/${MAX_FREE_DAILY_MESSAGES} messages` : ''}${dailyMessages === MAX_FREE_DAILY_MESSAGES ? '\n<b>You have reached six hours message limit. Please Upgrade to another plan to continue!</b>' : ''}
+
+      ${plan !== 'PRO' ? `🆙 Upgrade for a <b>PRO</b> plan to get boosts up to 1 month with no limits!` : ''}
+
+      --
+      Our other products:
+      <a href="https://t.me/BetterCallSOL_LOUNGE">@Better Call SOL | Lounge</a>
+      <a href="https://t.me/BetterCallSol_WalletTrackerBot">@Better Call SOL | Wallet Tracker</a>
+      <a href="https://t.me/BetterCallSol_InsidersBot">@Better Call SOL | Insiders</a>
+`);
+
+    return messageText;
+  }
+
+  public static insufficientBalanceMessage(): string {
+    const messageText = dedent(`
+      ❌ Ooops it seems that you don't have sufficient balance to perform this transaction.
+
+      You can try by adding some <b>SOL</b> to your Better Call SOL personal wallet 
+`);
+
+    return messageText;
+  }
+
+  public static walletLimitMessageError(
+    walletName: string | undefined,
+    walletAddress: string,
+    planWallets: number,
+  ): string {
+    const messageText = dedent(`
+      ❌ Could not add wallet: <code>${walletName ? walletName : walletAddress}</code>, 
+
+      Wallet limit reached: <b>${planWallets}</b>
+
+      You can try by upgrading your <b>plan</b> for more wallets 💎
+`);
+
+    return messageText;
+  }
+
+  public static botWalletError(): string {
+    const messageText = dedent(`
+  🔴 Oops! it seems that this wallet has been banned due to too many tps
+`);
+
+    return messageText;
+  }
+
+  public static botWalletBadFormatError(): string {
+    const messageText = dedent(`
+  🔴 Oops! Bad format of wallets!
+`);
+
+    return messageText;
+  }
+
+  public static botInternalServerError(): string {
+    const messageText = dedent(`
+  🟡 Better Call SOL Unexpected issue!
+
+We have encountered an unexpected issue affecting the Better Call SOL. We sincerely apologize for any inconvenience this may cause
+`);
+
+    return messageText;
+  }
+
+  public static botUnavailableError(): string {
+    const messageText = dedent(`
+  🔴 Better Call SOL is Currently Unavailable!
+
+We regret to inform you that Better Call SOL is temporarily down. We sincerely apologize for any inconvenience this may cause and appreciate your understanding as we work diligently to resolve the issue.
+
+Thank you for your patience. We will provide updates as soon as the service is restored.
+
+— The Better Call SOL Team
+`);
+
+    return messageText;
+  }
+
+  public static generalTransactionMessageError(): string {
+    const messageText = dedent(`
+    🟡 Ooops it seems that something went wrong while processing the transaction.
+
+    You probaly don't have sufficient balance in your wallet
+
+    Maybe try adding some <b>SOL</b> to your better call sol personal wallet!
+`);
+
+    return messageText;
+  }
+}

VidmantasVarnelis/boosts

src/services/telegram/listeners/callback-query.listener.ts
@@ -1,0 +1,121 @@
+import { PrismaSubscriptionRepository, PrismaUserRepository } from '@repo/database/repositories';
+import { CallbackQuery } from 'telegraf/types';
+import { ApplicationPlatform } from '@repo/database';
+import { DonateCommand, SubscriptionCommand, UserWalletCommand } from '../commands';
+import { GeneralMessages } from '../messages';
+import { START_MENU } from '../lib/menus';
+import { userExpectingDonation } from '../lib/waiting-stage';
+import { TelegramService } from '../telegram.service';
+import { Config, DonationEnum } from '../../../constants';
+import { CallbackQueryDataEnum } from '../constants';
+
+export class CallbackQueryListener {
+  private readonly userWalletCommand: UserWalletCommand;
+  private readonly subscriptionCommand: SubscriptionCommand;
+  private readonly donateCommand: DonateCommand;
+
+  constructor(
+    private readonly config: Config,
+    private readonly telegramService: TelegramService,
+    private readonly prismaUserRepository: PrismaUserRepository,
+    private readonly prismaSubscriptionRepository: PrismaSubscriptionRepository,
+  ) {
+    this.donateCommand = new DonateCommand(
+      this.config,
+      this.telegramService,
+      this.prismaUserRepository,
+      this.prismaSubscriptionRepository,
+    );
+    this.subscriptionCommand = new SubscriptionCommand(
+      this.config,
+      this.telegramService,
+      this.prismaUserRepository,
+      this.prismaSubscriptionRepository,
+    );
+    this.userWalletCommand = new UserWalletCommand(
+      this.config,
+      this.telegramService,
+      this.prismaUserRepository,
+    );
+  }
+
+  public start() {
+    this.telegramService.onCallbackQuery(async (query) => {
+      const chatId = query.chat?.id;
+      const message = query.callbackQuery.message;
+
+      if (!chatId || !message) {
+        return;
+      }
+
+      const data = (query.callbackQuery as CallbackQuery & { data: string | undefined }).data;
+
+      try {
+        switch (data) {
+          case CallbackQueryDataEnum.MY_WALLET:
+            await this.userWalletCommand.addButtonHandler(message.chat.id, message.message_id);
+            break;
+          case CallbackQueryDataEnum.SHOW_PRIVATE_KEY:
+            await this.userWalletCommand.showPrivateKey(message.chat.id, message.message_id);
+            break;
+          case CallbackQueryDataEnum.UPGRADE:
+            await this.subscriptionCommand.addButtonHandler(message.chat.id, message.message_id);
+            break;
+          case CallbackQueryDataEnum.UPGRADE_HOBBY:
+            await this.subscriptionCommand.upgradePlan(
+              message.chat.id,
+              message.message_id,
+              'HOBBY',
+            );
+            break;
+          case CallbackQueryDataEnum.UPGRADE_PRO:
+            await this.subscriptionCommand.upgradePlan(message.chat.id, message.message_id, 'PRO');
+            break;
+          case CallbackQueryDataEnum.DONATE:
+            await this.donateCommand.addButtonHandler(message.chat.id, message.message_id);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.MINIMUM}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.MINIMUM);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.LOW}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.LOW);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.MEDIUM}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.MEDIUM);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.HIGH}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.HIGH);
+            break;
+          case `${CallbackQueryDataEnum.DONATE_ACTION}_${DonationEnum.PREMIUM}`:
+            await this.donateCommand.chargeDonation(message.chat.id, DonationEnum.PREMIUM);
+            break;
+          case CallbackQueryDataEnum.BACK_TO_MAIN_MENU: {
+            const user = await this.prismaUserRepository.getById(chatId.toString());
+
+            userExpectingDonation.delete(chatId);
+
+            await this.telegramService.editMessageText(
+              chatId,
+              message.message_id,
+              GeneralMessages.startMessage(
+                user,
+                ApplicationPlatform.BOOSTS,
+                this.config.freeDailyMessages.get(chatId.toString())?.count ?? 0,
+              ),
+              {
+                replyMarkup: START_MENU,
+                mode: 'HTML',
+              },
+            );
+
+            break;
+          }
+          default:
+            await this.telegramService.sendMessage(chatId, 'Unknown command.');
+        }
+      } catch {
+        await this.telegramService.sendMessage(chatId, GeneralMessages.internalServerError());
+      }
+    });
+  }
+}

src/services/telegram/messages/general.messages.ts
@@ -1,0 +1,92 @@
+import dedent from 'dedent';
+import { MAX_FREE_DAILY_MESSAGES } from '../../../constants/pricing';
+import { UserPrisma } from '../../../typings';
+import { ApplicationPlatform } from '@repo/database';
+
+export class GeneralMessages {
+  public static startMessage(
+    user: UserPrisma,
+    platform: ApplicationPlatform,
+    dailyMessages: number,
+  ): string {
+    const subscription = user?.userSubscriptions?.find((sub) => sub.platform === platform);
+    const plan = subscription?.plan || 'FREE';
+
+    const messageText = dedent(`
+      Better Call SOL | BOOSTS!
+      You're currently on <b>${plan}</b> plan${plan === 'FREE' ? `\nFor Free plan you have every 6 hours ${dailyMessages}/${MAX_FREE_DAILY_MESSAGES} messages` : ''}${dailyMessages === MAX_FREE_DAILY_MESSAGES ? '\n<b>You have reached six hours message limit. Please Upgrade to another plan to continue!</b>' : ''}
+
+      ${plan !== 'PRO' ? `🆙 Upgrade for a <b>PRO</b> plan to get boosts up to 1 month with no limits!` : ''}
+
+      --
+      Our other products:
+      <a href="https://t.me/BetterCallSOL_LOUNGE">@Better Call SOL | Lounge</a>
+      <a href="https://t.me/BetterCallSol_WalletTrackerBot">@Better Call SOL | Wallet Tracker</a>
+      <a href="https://t.me/BetterCallSol_InsidersBot">@Better Call SOL | Insiders</a>
+`);
+
+    return messageText;
+  }
+
+  public static insufficientBalanceMessage(): string {
+    const messageText = dedent(`
+      ❌ Ooops it seems that you don't have sufficient balance to perform this transaction.
+
+      You can try by adding some <b>SOL</b> to your Better Call SOL personal wallet 
+`);
+
+    return messageText;
+  }
+
+  public static walletLimitMessageError(
+    walletName: string | undefined,
+    walletAddress: string,
+    planWallets: number,
+  ): string {
+    const messageText = dedent(`
+      ❌ Could not add wallet: <code>${walletName ? walletName : walletAddress}</code>, 
+
+      Wallet limit reached: <b>${planWallets}</b>
+
+      You can try by upgrading your <b>plan</b> for more wallets 💎
+`);
+
+    return messageText;
+  }
+
+  public static internalServerError(): string {
+    const messageText = dedent(`
+  🟡 Better Call SOL Unexpected issue!
+
+We have encountered an unexpected issue affecting the Better Call SOL. We sincerely apologize for any inconvenience this may cause
+`);
+
+    return messageText;
+  }
+
+  public static unavailableError(): string {
+    const messageText = dedent(`
+  🔴 Better Call SOL is Currently Unavailable!
+
+We regret to inform you that Better Call SOL is temporarily down. We sincerely apologize for any inconvenience this may cause and appreciate your understanding as we work diligently to resolve the issue.
+
+Thank you for your patience. We will provide updates as soon as the service is restored.
+
+— The Better Call SOL Team
+`);
+
+    return messageText;
+  }
+
+  public static generalTransactionMessageError(): string {
+    const messageText = dedent(`
+    🟡 Ooops it seems that something went wrong while processing the transaction.
+
+    You probaly don't have sufficient balance in your wallet
+
+    Maybe try adding some <b>SOL</b> to your better call sol personal wallet!
+`);
+
+    return messageText;
+  }
+}